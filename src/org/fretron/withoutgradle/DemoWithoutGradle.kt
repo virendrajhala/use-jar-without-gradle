a864c8534fb23d8155e1bdcc18d4286e8c7cc6dd
Virendra Jhala <virendrapratapsinghjhala@gmail.com>
2021-09-21 11:56:38 +0530

    Update DemoWithoutGradle.kt

--- src/org/fretron/withoutgradle/DemoWithoutGradle.kt
+++ src/org/fretron/withoutgradle/DemoWithoutGradle.kt
@@ -1,4 +1,6 @@
 package org.fretron.withoutgradle
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.*
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 
 data class User(val id: Int,
@@ -6,7 +8,11 @@ data class User(val id: Int,
                 val password: String,
                 val lastName: String){
 
-    private val mapper = jacksonObjectMapper()
+    private val mapper : ObjectMapper
+
+    init{
+        mapper = jacksonObjectMapper()
+    }
 
     override fun toString(): String {
 
